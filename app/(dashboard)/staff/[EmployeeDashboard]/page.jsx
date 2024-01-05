108cf638ac11d08556374f34fd0cf82a3394638f
buneshwar1313 <bmtendulkar@gmail.com>
2024-01-05 22:32:49 +0530

    changes in code

--- app/(dashboard)/staff/[EmployeeDashboard]/page.jsx
+++ app/(dashboard)/staff/[EmployeeDashboard]/page.jsx
@@ -1,10 +1,14 @@
+"use client"
 import React from 'react'
+import employeeDashboard from '@/components/Dashboard/employeeDashboard/employeeDashboard';
 
 const page = (params) => {
   const id = params.EmployeeDashboard;
   console.log(id)
   return (
-    <div>hii {id}</div>
+    <>
+    <employeeDashboard></employeeDashboard>
+    </>
   )
 }
 
